79222effad00776693e9d7a0b7d3b78e6bf05590
Ben <thisbenrogers@gmail.com>
2019-08-26 09:46:32 -0500

    :construction: Adds Login
    Adds form hook, Private Route, and recieves a token from localhost

--- potluck-planner/src/App.js
+++ potluck-planner/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
+import Login from './components/Login';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Potluck Planner</h1>
+      <Login />
     </div>
   );
 }
---
+++ potluck-planner/src/components/Login.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import axios from 'axios';
+
+import { useForm } from '../hooks/handleHook';
+
+const Login = () => {
+
+    const login = () => {
+        console.log("fields in Login: ", fields);
+
+        axios
+            .post('http://localhost:5000/api/login', {...fields})
+            .then(res => {
+                localStorage.setItem('token', res.data.payload)
+            })
+            .catch(err => console.error(err.response));
+    }
+
+    const { fields, submit, handleChanges } = useForm(() => login());
+
+    const password = 'i<3Lambd4';
+
+    return (
+        <>
+            <form onSubmit={submit}>
+                <input type="text" name='username' onChange={handleChanges} />
+                <p>Lambda School</p>
+                <input type="password" name='password' onChange={handleChanges} />
+                <p>{password}</p>
+                <button>Login</button>
+            </form>
+        </>
+    )
+}
+export default Login;
